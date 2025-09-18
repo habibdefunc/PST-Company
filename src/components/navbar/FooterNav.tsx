7b2c88f1bf155a3a7dc14c907b8753a5082bce19
Habib Ritonga <128818642+habibdefunc@users.noreply.github.com>
2025-09-18 19:48:29 +0700

    Fix Bug React-router Refresh And Footer Nav

--- src/components/navbar/FooterNav.tsx
+++ src/components/navbar/FooterNav.tsx
@@ -1,25 +1,25 @@
 import { menus } from "./menudata";
+import { useLocation, NavLink } from "react-router-dom";
 
 export default function FooterNav() {
-  const currentPath =
-    typeof window !== "undefined" ? window.location.pathname : "/";
+  const location = useLocation();
 
   return (
     <nav>
       <h3 className="text-lg font-semibold mb-3">Navigasi</h3>
       <ul className="flex flex-col md:flex-col gap-2 text-sm text-gray-300">
         {menus.map((menu) => {
-          const isActive = currentPath === menu.path;
+          const isActive = location.pathname === menu.path;
           return (
             <li key={menu.name}>
-              <a
-                href={menu.path}
+              <NavLink
+                to={menu.path}
                 className={`block hover:text-yellow-500 transition ${
                   isActive ? "text-yellow-500 font-semibold" : ""
                 }`}
               >
                 {menu.name}
-              </a>
+              </NavLink>
             </li>
           );
         })}
